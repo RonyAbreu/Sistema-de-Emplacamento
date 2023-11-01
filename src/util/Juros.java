0f764c32dca930894081bd217d83071b2c76af48
RonyAbreu <ronyelison78@gmail.com>
2023-11-01 20:57:34 -0300

    criando classe para controlar os Juros das parcelas

---
+++ src/util/Juros.java
@@ -0,0 +1,6 @@
+package util;
+public class Juros {
+    public static final Double JUROS_DE_2_PARCELAS = 0.2;
+    public static final Double JUROS_DE_3_PARCELAS = 0.3;
+    public static final Double JUROS_DE_4_PARCELAS = 0.4;
+}
