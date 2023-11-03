d19a97ab6d2be5fd4a2e25fee21c489ad3565f10
RonyAbreu <ronyelison78@gmail.com>
2023-11-02 21:47:23 -0300

    criação da classe para persistir os dados

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import db.BancoDeDados;
 import entity.RegistroClientes;
 import gui.SistemaEmplacamentoTela;
+
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
