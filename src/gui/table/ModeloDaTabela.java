974414b0609c0d8fa3ec2300865af44e3631451e
RonyAbreu <ronyelison78@gmail.com>
2023-11-03 23:37:03 -0300

    criando funcionalidade de popular a tabela de clientes

---
+++ src/gui/table/ModeloDaTabela.java
@@ -0,0 +1,52 @@
+package gui.table;
+
+import entity.Cliente;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class ModeloDaTabela extends AbstractTableModel {
+    private final String[] coluna = {"NOME","TELEFONE","PLACA","PARCELAS","VALOR DA PARCELA","VALOR TOTAL"};
+    private List<Cliente> listaDeClientes;
+
+    public ModeloDaTabela(List<Cliente> listaDeClientes) {
+        this.listaDeClientes = listaDeClientes;
+    }
+
+    @Override
+    public int getRowCount() {
+        return listaDeClientes.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return coluna.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return switch (columnIndex){
+            case 0 -> listaDeClientes.get(rowIndex).getNome();
+            case 1 -> listaDeClientes.get(rowIndex).getTelefone();
+            case 2 -> listaDeClientes.get(rowIndex).getEmplacamento().getNomeDaPlaca();
+            case 3 -> listaDeClientes.get(rowIndex).getEmplacamento().getQuantidadeDeParcelas();
+            case 4 -> listaDeClientes.get(rowIndex).getEmplacamento().getValorDaParcela();
+            case 5 -> listaDeClientes.get(rowIndex).getEmplacamento().getValorTotal();
+            default -> "-";
+        };
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return coluna[column];
+    }
+
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        if (getValueAt(0,columnIndex) != null){
+            return getValueAt(0, columnIndex).getClass();
+        } else {
+            return Object.class;
+        }
+    }
+}
