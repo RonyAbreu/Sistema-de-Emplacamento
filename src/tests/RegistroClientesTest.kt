9575c13b5ac7ee74d4fcb63763c40f5a72e277a3
João Vitor Avelar <jvitoravelar1200@gmail.com>
2023-11-05 14:27:31 -0300

    Criada classe de testes da classe RegistroClientes

--- src/tests/RegistroClientesTest.kt
+++ src/tests/RegistroClientesTest.kt
@@ -1,42 +1,99 @@
 package tests
 
-import org.junit.jupiter.api.DisplayName
+import entity.Cliente
+import entity.InfoEmplacamento
+import entity.Parcela
+import entity.RegistroClientes
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+import java.time.LocalDate
 
 class RegistroClientesTest {
 
+
     @Test
-    @DisplayName("Teste Cadastrar Clientes")
     fun cadastrarClientes() {
+        val cliente1 = Cliente("Joao", "8888", "",
+            emplacamento = InfoEmplacamento("placa", 300.0,
+                50.0,2)
+        )
+        val cliente2 = Cliente("Rony", "3333", "",
+            emplacamento = InfoEmplacamento("ronyboy", 500.0,
+                100.0,2)
+        )
+
+        val lista = ArrayList<Cliente>()
+/*        lista.add(cliente1)
+        lista.add(cliente2)*/
+
+        val registroClientes = RegistroClientes(lista)
+
+        registroClientes.cadastrarClientes(cliente1)
+        registroClientes.cadastrarClientes(cliente2)
+
     }
 
     @Test
-    @DisplayName("Teste Pesquisar Clientes pelo nome")
-    fun pesquisarClientePeloNome() {
-    }
-
-    @Test
-    @DisplayName("Teste Pesquisar lista Clientes")
     fun pesquisarListaClientes() {
+
+        val cliente = Cliente("Joao", "8888", "",
+            emplacamento = InfoEmplacamento("placa", 300.0,
+                50.0,2)
+        )
+
+        val listaClientes = ArrayList<Cliente>()
+        listaClientes.add(cliente)
+
+        val registroClientes = RegistroClientes(listaClientes)
+
+        Assertions.assertEquals( mutableListOf(cliente).isNotEmpty(), registroClientes.pesquisarListaClientes(cliente.nome).isNotEmpty() )
+
+        // Certamente ele me retorna uma lista de clientes!
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun pesquisarPelaPlaca() {
+
+        val cliente = Cliente("Joao", "8888", "",
+            emplacamento = InfoEmplacamento("placa", 300.0,
+                50.0,2)
+        )
+
+        val listaClientes = ArrayList<Cliente>()
+        listaClientes.add(cliente)
+
+        val registroClientes = RegistroClientes(listaClientes)
+
+        assertEquals( cliente , registroClientes.pesquisarPelaPlaca(cliente.emplacamento.nomeDaPlaca) )
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun pesquisarClientesComPagamentoVencido() {
+
+        val cliente = Cliente("Joao", "8888", "",
+            emplacamento = InfoEmplacamento("placa", 300.0,
+                50.0,2)
+        )
+
+        val listaClientes = ArrayList<Cliente>()
+        listaClientes.add(cliente)
+
+        val pagamento = Parcela(dataDeVencimento = LocalDate.of(2023,10,9))
+        val registroClientes = RegistroClientes(listaClientes)
+
+        assertEquals( pagamento.validaDataVencimento(), cliente.emplacamento.retornaListaDeParcelasVencidas().isEmpty())
+
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun deletarClientePeloNome() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     fun retornarTodosOsClientes() {
-    }
-
-    @org.junit.jupiter.api.Test
-    fun getListaDeClientes() {
     }
 }
